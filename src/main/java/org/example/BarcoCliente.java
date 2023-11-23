a421e30ff9cfaa0159a03e2b5c56b5795402daa4
florencia17 <flormalacarne@gmail.com>
2023-11-23 20:46:46 -0300

    funcionando

--- src/main/java/org/example/BarcoCliente.java
+++ src/main/java/org/example/BarcoCliente.java
@@ -1,4 +1,46 @@
 package org.example;
 
-public class Cliente {
+import java.io.*;
+import java.net.Socket;
+
+public class BarcoCliente {
+    private static final String SERVER_IP = "localhost";
+    private static final int SERVER_PORT = 9876;
+    private int id;
+    private String direction;
+
+    public BarcoCliente(int id, String direction) {
+        this.id = id;
+        this.direction = direction;
+    }
+
+    public static void main(String[] args) {
+        int numBarcos = 8;
+
+        for (int i = 0; i < numBarcos; i++) {
+            String direction = (i % 2 == 1) ? "OESTE" : "ESTE";
+            int id = i;
+            new Thread(() -> new BarcoCliente(id, direction).realizarNavegacion()).start();
+        }
+    }
+
+    public void realizarNavegacion() {
+        try (Socket socket = new Socket(SERVER_IP, SERVER_PORT)) {
+            System.out.println("Barco:" + id + " " + direction + ": Conectado al servidor de esclusas.");
+
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+            writer.println("Quiere cruzar" + id + ": " + direction);
+
+            System.out.println("Barco:" + id + ": Navegación no permitida. Esperando un barco mas.");
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String line = reader.readLine();
+
+            if (line.equals("NAVEGACION_PERMITIDA"))
+                System.out.println("Barco:" + id + ": Navegacion permitida.");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/org/example/ServidorExclusas.java
+++ src/main/java/org/example/ServidorExclusas.java
@@ -1,2 +1,77 @@
-package org.example;public class ServidorExclusas {
+package org.example;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class ServidorExclusas {
+    private static final int PORT = 9876;
+    private static final Queue<Socket> oesteQueue = new ConcurrentLinkedQueue<>();
+    private static final Queue<Socket> esteQueue = new ConcurrentLinkedQueue<>();
+    private static final Object lock = new Object();
+
+    public static void main(String[] args) {
+        ServidorExclusas server = new ServidorExclusas();
+        server.iniciarServidor();
+    }
+
+    public void iniciarServidor() {
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.println("Servidor de Exclusas iniciado en el puerto " + PORT + " - Esperando Barcos - ");
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                new Thread(() -> gestionarEsclusas(clientSocket)).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void gestionarEsclusas(Socket clientSocket) {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
+
+            String request = reader.readLine();
+            String[] parts = request.split(":");
+            String direction = parts[1].trim();
+
+            synchronized (lock) {
+                if (direction.equals("OESTE")) {
+                    oesteQueue.add(clientSocket);
+                    System.out.println("Nuevo barco en esclusa OESTE. Total en el Oeste: " + oesteQueue.size());
+                } else {
+                    esteQueue.add(clientSocket);
+                    System.out.println("Nuevo barco en esclusa ESTE. Total en el Este:" + esteQueue.size());
+                }
+
+                if (oesteQueue.size() == 2) {
+                    permitirPaso(oesteQueue);
+                } else if (esteQueue.size() == 2) {
+                    permitirPaso(esteQueue);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void permitirPaso(Queue<Socket> queue) {
+        Socket ship1 = queue.poll();
+        Socket ship2 = queue.poll();
+
+        try {
+            PrintWriter writer1 = new PrintWriter(ship1.getOutputStream(), true);
+            PrintWriter writer2 = new PrintWriter(ship2.getOutputStream(), true);
+
+            writer1.println("NAVEGACION_PERMITIDA");
+            writer2.println("NAVEGACION_PERMITIDA");
+
+            System.out.println("-- Navegando --");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
